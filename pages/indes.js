102abd62e660dc7e7133715b0b5db81f15566ad8
boukiwolof-coder <boukiwolof@gmail.com>
2025-09-09 20:41:50 -0400

    Create indes.js

---
+++ pages/indes.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:'center', padding:'2rem'}}>
+      <h1>BoukiCoin: Rise Again with $BOUKI</h1>
+      <p>Website coming soon…</p>
+      <a href="https://pump.fun/coin/C26rhxCo2ZNJn8RDzLEsucBozE1X53eLV4ygeasdpump" target="_blank" rel="noopener noreferrer">
+        Buy $BOUKI
+      </a>
+    </div>
+  )
+}
